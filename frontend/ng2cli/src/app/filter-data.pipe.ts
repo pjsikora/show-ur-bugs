7f7ae0be760b3647a69a606cd4c3fd97069531e4
piotr <fedojo.com@gmail.com>
2016-06-13 22:15:20 +0200

    Pipe draft

--- frontend/ng2cli/src/app/filter-data.pipe.ts
+++ frontend/ng2cli/src/app/filter-data.pipe.ts
@@ -6,8 +6,24 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 
 export class FilterData implements PipeTransform {
-    transform(items:any[], args:string[]):any[] {
+    transform(items:any[], args:string[], additional):any[] {
+
+
+
+
+
         if (typeof items === 'object') {
+            var paramNames = ['all'];
+
+
+            if (typeof additional !== 'undefined') {
+                console.log('undefined');
+
+                console.log("parameters: "+paramNames);
+            }
+
+            console.log(items[0]['name']);
+
             var resultArray = [];
             if (args.length === 0) {
                 resultArray = items;
@@ -15,10 +31,14 @@ export class FilterData implements PipeTransform {
 
             else {
                 for (let item of items) {
-                    Object.getOwnPropertyNames(item).forEach(function(element) {
-                        console.log(element);
-                        console.log(item);
-                    });
+                    // if (item.hasOwnProperty(paramNames)) {
+                    //     console.log(item[paramNames]);
+                    // }
+                    
+                    // Object.getOwnPropertyNames(item).forEach(function(element) {
+                    //     console.log(element);
+                    //     console.log(item);
+                    // });
                     if (item.name != null && item.name.match(new RegExp(''+args, 'i'))) {
                         // console.log(item.name);
                         resultArray.push(item);
