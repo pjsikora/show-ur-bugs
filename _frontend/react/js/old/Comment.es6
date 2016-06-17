df111c55c62f5851c1bdf4a886bba454b5d89640
piotr <fedojo.com@gmail.com>
2016-06-17 06:13:12 +0200

    Cleaning

--- _frontend/react/js/old/Comment.es6
+++
@@ -1,9 +0,0 @@
-class Comment {
-    constructor(user, date, parent, content) {
-        this.user = user;
-        this.date = date;
-        this.parent = parent;
-        this.content = content;
-    }
-}
-
--- _frontend/react/js/old/main.js
+++
@@ -1,9 +0,0 @@
-var points = [];
-var mode = [];
-var ls = window.localStorage;
-var user = 'piotr@nitro-digital.com';
-var project = 'test';
-
-document.addEventListener('DOMContentLoaded', function (e){
-    var view = new View('img/NitroDigitalPoland_Praktyki.jpg');
-});
